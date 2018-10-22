035b5494338e8a3c798bd522327a97b4cfec7b8f
Sergey Lukashev <lukashev.s@ya.ru>
2018-10-22 15:32:54 +0300

    fixed build

--- de_airsense/src/de_airsense/nodes/__init__.py
+++ de_airsense/src/de_airsense/nodes/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import mission_px4, mission_dji
+import mission_px4, mission_dji, waspmote_ipfs
 
 
 def mission_dji_node():
@@ -8,3 +8,7 @@ def mission_dji_node():
 
 def mission_px4_node():
     mission_px4.FlightMission().spin()
+
+
+def waspmote_node():
+    waspmote_ipfs.WaspmoteIPFS().spin()
