325bc2cd71f97a60b974cf316e6c1bfd9385803f
Sergey Lukashev <lukashev.s@ya.ru>
2018-12-21 20:19:13 +0300

    px4 works with utraffic

--- de_airsense/src/de_airsense/nodes/mission_px4.py
+++ de_airsense/src/de_airsense/nodes/mission_px4.py
@@ -2,28 +2,28 @@
 
 from time import sleep
 import rospy
-from mavros_msgs.msg import Waypoint, WaypointList
-from mavros_msgs.srv import *
+from mavros_msgs.msg import Waypoint, WaypointList, ExtendedState, CommandCode
+from mavros_msgs.srv import SetMode, WaypointClear, WaypointPush, CommandBool, CommandTOL
 from sensor_msgs.msg import NavSatFix, Imu
 from de_msgs.msg import Mission
+from std_msgs.msg import Empty
+
 
 class FlightMission(object):
-
-    MAV_GLOBAL_FRAME = 3
-    MAV_CMD_WAYPOINT = 16
-    MAV_CMD_RTL = 20
-    MAV_CMD_LAND = 21
-    MAV_CMD_TAKEOFF = 22
-
-    lat = 0.0
-    lon = 0.0
+    lat = 0.
+    lon = 0.
+    alt = 0.
 
     def __init__(self):
         rospy.init_node('de_airsense_mission')
         rospy.loginfo('Waiting mavros for services...')
 
+        self.__last_state = ExtendedState.LANDED_STATE_ON_GROUND
+
         self.__gpsPublisher = rospy.Publisher('de/drone/gps_position', NavSatFix, queue_size=100)
         self.__imuPublisher = rospy.Publisher('de/drone/imu', Imu, queue_size=100)
+        self.__landed_pub = rospy.Publisher('de/drone/landing', Empty, queue_size=100)
+        self.__takeoff_pub = rospy.Publisher('de/drone/takeoff', Empty, queue_size=100)
 
         def push_mission(waypoints):
             rospy.wait_for_service('mavros/mission/clear')
@@ -61,8 +61,8 @@ class FlightMission(object):
 
             wl = WaypointList()
             wp = Waypoint()
-            wp.frame = self.MAV_GLOBAL_FRAME
-            wp.command = self.MAV_CMD_TAKEOFF  # takeoff
+            wp.frame = Waypoint.FRAME_GLOBAL
+            wp.command = CommandCode.NAV_TAKEOFF  # takeoff
             wp.is_current = True
             wp.autocontinue = True
             wp.param1 = mission_msg.waypoints[0].altitude # takeoff altitude
@@ -76,8 +76,8 @@ class FlightMission(object):
 
             for item in mission_msg.waypoints:
                 wp = Waypoint()
-                wp.frame = self.MAV_GLOBAL_FRAME
-                wp.command = self.MAV_CMD_WAYPOINT  # simple point
+                wp.frame = Waypoint.FRAME_GLOBAL
+                wp.command = CommandCode.NAV_WAYPOINT  # simple point
                 wp.is_current = False
                 wp.autocontinue = True
                 wp.param1 = 0  # takeoff altitude
@@ -90,12 +90,12 @@ class FlightMission(object):
                 wp.z_alt = item.altitude
                 wl.waypoints.append(wp)
 
-            wp = Waypoint()
-            wp.frame = 2 
-            wp.command = self.MAV_CMD_RTL
-            wp.is_current = False
-            wp.autocontinue = True
-            wl.waypoints.append(wp)
+            # wp = Waypoint()
+            # wp.frame = 2
+            # wp.command = CommandCode.NAV_RETURN_TO_LAUNCH
+            # wp.is_current = False
+            # wp.autocontinue = True
+            # wl.waypoints.append(wp)
 
             push_mission(wl.waypoints)
 
@@ -112,7 +112,8 @@ class FlightMission(object):
         def gps_cb(data): 
             self.lat = data.latitude
             self.lon = data.longitude
-            rospy.loginfo_throttle(1, 'position: %s, %s' % (self.lat, self.lon))
+            self.alt = data.altitude
+            rospy.loginfo_throttle(1, 'position: %s, %s, %s' % (self.lat, self.lon, self.alt))
             self.__gpsPublisher.publish(data)
 
         rospy.Subscriber("mavros/global_position/global", NavSatFix, gps_cb)
@@ -129,6 +130,39 @@ class FlightMission(object):
             rospy.loginfo('Flight!')
 
         rospy.Subscriber('de/drone/mission', Mission, mission_cb)
+
+        def state_cb(msg):
+            rospy.loginfo(msg.landed_state)
+            if msg.landed_state != self.__last_state:
+                if msg.landed_state == ExtendedState.LANDED_STATE_ON_GROUND:
+                    self.__landed_pub.publish(Empty())
+                    rospy.loginfo('landing detected')
+                elif msg.landed_state == ExtendedState.LANDED_STATE_IN_AIR:
+                    self.__takeoff_pub.publish(Empty())
+                    rospy.loginfo('takeoff detected')
+
+                self.__last_state = msg.landed_state
+
+        rospy.Subscriber('mavros/extended_state', ExtendedState, state_cb)
+
+        def land_cb(msg):
+            rospy.loginfo('land cmd issued')
+            sleep(3)
+
+            try:
+                land_service = rospy.ServiceProxy('mavros/cmd/land', CommandTOL)
+                land_service(0, 0, self.lat, self.lon, 0)
+            except rospy.ServiceException as e:
+                rospy.loginfo('Service call failed: {0}'.format(e))
+
+        def takeoff_cb(msg):
+            rospy.loginfo('takeoff cmd issued')
+            set_mode('AUTO.TAKEOFF')
+            sleep(5)
+            set_mode('AUTO.MISSION')
+
+        rospy.Subscriber('de/drone/cmd/land', Empty, land_cb)
+        rospy.Subscriber('de/drone/cmd/takeoff', Empty, takeoff_cb)
 
     def spin(self):
         rospy.spin()
